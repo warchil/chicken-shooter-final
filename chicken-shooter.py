013d6c9ea6b9dac12460fd1d9cf376b5608b8429
warchil <warchil.m@seznam.cz>
2023-03-28 15:22:31 +0200

    Kod upraveny podle prace v hodine

--- chicken-shooter.py
+++ chicken-shooter.py
@@ -9,6 +9,9 @@ pygame.init()
 pygame.mixer.init()
 zvukVystrelu = pygame.mixer.Sound('zvuky/pistol.wav')
 zvukZasahuSlepice = pygame.mixer.Sound('zvuky/zasah.wav')
+zvukNabiti = pygame.mixer.Sound('zvuky/nabiti.wav')
+zvukPrazdnehoZasobniku = pygame.mixer.Sound('zvuky/prazdny-zasobnik.wav')
+hudbaNaPozadi = pygame.mixer.Sound('zvuky/music.mp3')
 
 font = pygame.font.Font("freesansbold.ttf", 32)
 
@@ -29,27 +32,50 @@ hodiny = pygame.time.Clock()
 text = font.render("Skore: " + str(skore), True, pygame.Color("white"))
 text_rect = text.get_rect()
 
-prvniSlepice = slepice.Slepice(100, 5)
+# prvniSlepice = slepice.Slepice(100, 5)
+
+mnozinaSlepic = {
+    slepice.Slepice(random.randint(75, 150), random.randint(3, 15)),
+    slepice.Slepice(random.randint(75, 150), random.randint(3, 15)),
+    slepice.Slepice(random.randint(75, 150), random.randint(3, 15))
+}
+
+pygame.mixer.Channel(7).play(hudbaNaPozadi, -1)
+
+aktualniZasobnik = zasobnik.Zasobnik(8)
 
 hraBezi = True
 while hraBezi:
 
     for event in pygame.event.get():
+        # Pokud hrac kliknul na krizek, tak se nastavi promenna "hraBezi" na False (Nepravda)
         if event.type == pygame.QUIT:
             hraBezi = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.mixer.Channel(0).play(zvukVystrelu)
-            if prvniSlepice.zastrelena(obrazekZamerovace_rect):
-                skore = skore + 1
-                text = font.render("Skore: " + str(skore), True, pygame.Color("white"))
-                text_rect = text.get_rect()
-                pygame.mixer.Channel(0).play(zvukZasahuSlepice)
-                prvniSlepice.restartuj_se()
+            if (event.button == 1):
+                if (aktualniZasobnik.aktualniPocetNaboju > 0):
+                    aktualniZasobnik.aktualniPocetNaboju = aktualniZasobnik.aktualniPocetNaboju - 1
+                    pygame.mixer.Channel(0).play(zvukVystrelu)
+                    for slepice in mnozinaSlepic:
+                        if slepice.zastrelena(obrazekZamerovace_rect):
+                            skore = skore + 1
+                            text = font.render("Skore: " + str(skore), True, pygame.Color("white"))
+                            text_rect = text.get_rect()
+                            pygame.mixer.Channel(0).play(zvukZasahuSlepice)
+                            slepice.restartuj_se()
+                else:
+                    pygame.mixer.Channel(0).play(zvukPrazdnehoZasobniku)
+            else:
+                pygame.mixer.Channel(0).play(zvukNabiti)
+                aktualniZasobnik.aktualniPocetNaboju = 8
 
     herniOkno.blit(pozadi, (0, 0))
 
-    prvniSlepice.posun_se()
-    prvniSlepice.nakresli_se(herniOkno)
+    aktualniZasobnik.nakresli_se(herniOkno)
+
+    for slepice in mnozinaSlepic:
+        slepice.posun_se()
+        slepice.nakresli_se(herniOkno)
 
     obrazekZamerovace_rect.center = pygame.mouse.get_pos()
     herniOkno.blit(obrazekZamerovace, obrazekZamerovace_rect)
